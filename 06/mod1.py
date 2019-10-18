1ded321fbfeff2281ee497ea2fc5034d232c5d07
Pang <jiampan@apac.corpdir.net>
2019-10-18 22:46:46 +0800

    4pwd

--- 06/mod1.py
+++ 06/mod1.py
@@ -6,13 +6,21 @@ Created on Fri Oct 18 20:11:07 2019
 """
 import random
 
-pwdGenSymbols = "!@#$%^&*()_+-=~`[]\;',./{}|:\"<>?0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+pwdGenSymbols = "!@#$%^&*()_+-=~`[]\;',./{}|:\"<>?"
+pwdGennum ="0123456789"
+pwdGencharsu ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+pwdGencharsl ="abcdefghijklmnopqrstuvwxyz"
+pwdGenFull=pwdGenSymbols+pwdGennum+pwdGencharsu+pwdGencharsl
 
 def genPassword(n):
     a = 0
     result = ""
-    while a < n:
-        result = result + pwdGenSymbols[random.randint(0,len(pwdGenSymbols)-1)]
+    result = result + pwdGenSymbols[random.randint(0,len(pwdGenSymbols)-1)]
+    result = result + pwdGennum[random.randint(0,len(pwdGennum)-1)]
+    result = result + pwdGencharsu[random.randint(0,len(pwdGencharsu)-1)]
+    result = result + pwdGencharsl[random.randint(0,len(pwdGencharsl)-1)]
+    while a < n - 4:
+        result = result + pwdGenFull[random.randint(0,len(pwdGenFull)-1)]
         a=a+1
     return result
 genPassword(12)
